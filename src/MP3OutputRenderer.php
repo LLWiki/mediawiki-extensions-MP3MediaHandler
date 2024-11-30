8478c30e33d46f1af8f0b3d38711d0d0aa766b94
Func <Funcer@outlook.com>
2024-11-30 21:15:20 +0800

    Do not preload at all

--- src/MP3OutputRenderer.php
+++ src/MP3OutputRenderer.php
@@ -27,7 +27,7 @@ class MP3OutputRenderer extends MediaTransformOutput {
 	 * @return string
 	 */
 	public function toHtml( $options = [] ) {
-		$Output = '<audio preload="metadata" controls="controls">'
+		$Output = '<audio preload="none" controls="controls">'
 				. '<source src="$1" type="audio/mp3" />'
 				. $this->getFlashPlayerHTMLTemplate( '<a href="$1">$2</a>', $this->pSourceFileURL )
 				. '</audio>';
